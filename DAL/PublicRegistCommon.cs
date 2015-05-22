0c912e6ec43bd6a58fd9f93abe85a5e4ae9566be
lbs201 <405186620@qq.com>
2015-05-22 14:29:20 +0800

    AddFlowAndPermission

---
+++ DAL/PublicRegistCommon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PublicRegistCommon : DALBase
+    {
+        /// <summary>
+        /// 按行业类型添加企业流程、流程明细、管理员录入追溯信息权限
+        /// </summary>
+        /// <param name="orgID">企业ID</param>
+        /// <param name="userID">管理员用户ID</param>
+        /// <param name="hangyeID">行业ID</param>
+        /// <returns></returns>
+        public bool AddFlowAndPermission(int orgID,int userID,int hangyeID)
+        {
+            bool result = false;
+            try
+            {
+                using (LinqModel.DataClassesZYZSDataContext dc = GetDataContext())
+                {
+                   
+                }
+            }
+            catch { result = false; }
+            return result;
+        }
+    }
+}
